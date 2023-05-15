22d59f5f66dcb489994671bc3686d4d04af74fc5
Frederik Harder <frederikharder@gmail.com>
2023-05-15 17:29:15 +0200

    ...

--- code/cluster_scripts/00_cifar10_test.sh
+++ code/cluster_scripts/00_cifar10_test.sh
@@ -4,7 +4,7 @@ case $1 in
 python3.9 dp_mepf.py --pytorch_encoders --n_iter 200_000 --valid_iter 5_000 --restart_iter 5_000 --syn_eval_iter 5_000 --dataset cifar10 --val_enc fid_features --val_data train --validation_mode static --exp_name cifar10_test/run_0 --dp_tgt_eps 0.2 --matched_moments m1_and_m2 --dp_val_noise_scaling 10. --batch_size 128 --lr 1e-3 --m_avg_lr 3e-4 --seed 1
 ;; 1 )
 python3.9 dp_mepf.py --pytorch_encoders --n_iter 200_000 --valid_iter 5_000 --restart_iter 5_000 --syn_eval_iter 5_000 --dataset cifar10 --val_enc fid_features --val_data train --validation_mode static --exp_name cifar10_test/run_1 --dp_tgt_eps 10. --matched_moments mean --dp_val_noise_scaling 10. --batch_size 128 --lr 1e-3 --m_avg_lr 3e-4 --seed 5
-
+;; esac
 
 CODE=$?
 if [ $CODE -eq 0 ]
