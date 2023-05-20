1fea988407283a519866b9e3fe5d5b5fe3667ff7
Frederik Harder <frederikharder@gmail.com>
2023-05-20 20:05:00 +0200

    ...

--- code/cluster_scripts/00_cifar10_test.sh
+++ code/cluster_scripts/00_cifar10_test.sh
@@ -4,9 +4,9 @@ module load cuda/11.3
 
 case $1 in
 0 )
-python3 dp_mepf.py --n_iter 200_000 --restart_iter 25_000 --ckpt_iter 25_000 --syn_eval_iter 5_000 --dataset cifar10 --val_enc fid_features --val_data train --exp_name cifar10_test/run_0 --dp_tgt_eps 0.2 --matched_moments m1_and_m2 --dp_val_noise_scaling 10. --batch_size 128 --lr 1e-3 --m_avg_lr 3e-4 --seed 1
+python3.9 dp_mepf.py --n_iter 200_000 --restart_iter 25_000 --ckpt_iter 25_000 --syn_eval_iter 5_000 --dataset cifar10 --val_enc fid_features --val_data train --exp_name cifar10_test/run_0 --dp_tgt_eps 0.2 --matched_moments m1_and_m2 --dp_val_noise_scaling 10. --batch_size 128 --lr 1e-3 --m_avg_lr 3e-4 --seed 1
 ;; 1 )
-python3 dp_mepf.py --n_iter 200_000 --restart_iter 25_000 --ckpt_iter 25_000 --syn_eval_iter 5_000 --dataset cifar10 --val_enc fid_features --val_data train --exp_name cifar10_test/run_1 --dp_tgt_eps 10. --matched_moments mean --dp_val_noise_scaling 10. --batch_size 128 --lr 1e-3 --m_avg_lr 3e-4 --seed 5
+python3.9 dp_mepf.py --n_iter 200_000 --restart_iter 25_000 --ckpt_iter 25_000 --syn_eval_iter 5_000 --dataset cifar10 --val_enc fid_features --val_data train --exp_name cifar10_test/run_1 --dp_tgt_eps 10. --matched_moments mean --dp_val_noise_scaling 10. --batch_size 128 --lr 1e-3 --m_avg_lr 3e-4 --seed 5
 ;; esac
 
 CODE=$?
