bc8b8c2d8517982e52e4570d733448dd1e9b4905
Frederik Harder <frederikharder@gmail.com>
2023-06-07 14:05:02 +0200

    ...

--- code/dpgan_backpack.py
+++ code/dpgan_backpack.py
@@ -20,6 +20,46 @@ REAL_LABEL = 1.
 FAKE_LABEL = 0.
 
 
+def get_args():
+  parser = argparse.ArgumentParser()
+
+  # PARAMS YOU LIKELY WANT TO SET
+  parser.add_argument('--seed', type=int, default=999)
+  parser.add_argument('--data', type=str, default='cifar10',
+                      choices=['cifar10', 'celeba', 'imagenet32', 'celeba64', 'imagenet64'])
+  parser.add_argument('--workers', type=int, default=2)
+  parser.add_argument('--batch_size', type=int, default=128)
+  parser.add_argument('--batch_size_grad_acc', type=int, default=128)
+  parser.add_argument('--nc', type=int, default=3)
+  parser.add_argument('--nz', type=int, default=100)
+  parser.add_argument('--ngf', type=int, default=64)
+  parser.add_argument('--ndf', type=int, default=64)
+
+  parser.add_argument('--n_epochs', type=int, default=20)
+  parser.add_argument('--n_save_epochs', type=int, default=5)
+  parser.add_argument('--lr_default', type=float, default=0.0002)
+  parser.add_argument('--lr_dis', type=float, default=None)
+  parser.add_argument('--lr_gen', type=float, default=None)
+  parser.add_argument('--beta1', type=float, default=0.5)
+  parser.add_argument('--n_gpu', type=int, default=1)
+  parser.add_argument('--train_gen_every_n_steps', '-gen_freq', type=int, default=1)
+
+  parser.add_argument('--target_eps', type=float, default=None)
+  parser.add_argument('--target_delta', type=float, default=1e-6)
+  parser.add_argument('--clip_norm', type=float, default=None)
+
+  parser.add_argument('--model', type=str, default='convnet', choices=['convnet', 'resnet'])
+  parser.add_argument('--exp_name', type=str)
+  parser.add_argument('--pretrain_checkpoint', type=str, default=None)
+  parser.add_argument('--single_iter', action='store_true')
+  parser.add_argument('--local_data', action='store_true')
+
+  parser.add_argument('--gen_output', type=str, default='tanh', choices=['linear', 'tanh'])
+
+  arg = parser.parse_args()
+  return arg
+
+
 # custom weights initialization called on netG and netD
 def weights_init(m):
     classname = m.__class__.__name__
@@ -242,43 +282,79 @@ def get_gen_noise(b_size, nz, device):
   return pt.randn(b_size, nz, 1, 1, device=device)
 
 
+def create_checkpoint(log_dir, epoch, noise_scale, best_fid, best_epoch, best_syn_data_path,
+                      opt_g, opt_d, net_g, net_d, global_step):
+  ckpt = dict()
+  ckpt['epoch'] = epoch
+  ckpt['noise_scale'] = noise_scale
+  ckpt['best_fid'] = best_fid
+  ckpt['best_epoch'] = best_epoch
+  ckpt['best_syn_data_path'] = best_syn_data_path
+  ckpt['global_step'] = global_step
+  ckpt['opt_g'] = opt_g.state_dict()
+  ckpt['opt_d'] = opt_d.state_dict()
+  ckpt['net_g'] = net_g.state_dict()
+  ckpt['net_d'] = net_d.state_dict()
+
+
+  pt.save(ckpt, os.path.join(log_dir, f'checkpoint.pt'))
+  pt.save(ckpt, os.path.join(log_dir, f'checkpoint_ep{epoch}.pt'))
+
+
+def load_checkpoint(checkpoint_path):
+  ckpt = pt.load(checkpoint_path)
+  start_epoch = ckpt['epoch'] + 1
+  noise_scale = ckpt['noise_scale']
+  best_fid = ckpt['best_fid']
+  best_epoch = ckpt['best_epoch']
+  best_syn_data_path = ckpt['best_syn_data_path']
+  opt_g_state = ckpt['opt_g']
+  opt_d_state = ckpt['opt_d']
+  net_g_state = ckpt['net_g']
+  net_d_state = ckpt['net_d']
+  global_step = ckpt['global_step']
+  return start_epoch, noise_scale, best_fid, best_epoch, best_syn_data_path, \
+      opt_g_state, opt_d_state, net_g_state, net_d_state, global_step
+
+
+def print_fake_batch(arg, fake, data_scale, save_dir, file_name='clamped_plot.png'):
+  if arg.data in {'cifar10', 'celeba'}:
+    if data_scale == 'normed':
+      mean_tsr = pt.tensor(IMAGENET_MEAN, device=fake.device)
+      sdev_tsr = pt.tensor(IMAGENET_SDEV, device=fake.device)
+      data_to_print = fake * sdev_tsr[None, :, None, None] + mean_tsr[None, :, None, None]
+    else:
+      data_to_print = fake
+  else:
+    mean_tsr = pt.tensor([0.5, 0.5, 0.5], device=fake.device)
+    sdev_tsr = pt.tensor([0.5, 0.5, 0.5], device=fake.device)
+    data_to_print = fake * sdev_tsr[None, :, None, None] + mean_tsr[None, :, None, None]
+
+  print(f'data value range prior to clamping: min={pt.min(data_to_print)}, max={pt.max(data_to_print)}')
+  data_to_print = pt.clamp(data_to_print, min=0., max=1.)
+  img_path_clamp = os.path.join(save_dir, file_name)
+  vutils.save_image(data_to_print, img_path_clamp, normalize=True, nrow=10)
+
+
+def update_best_score(fid, syn_data_path, epoch, best_fid, best_syn_data_path, best_epoch):
+  if best_fid is None or fid < best_fid:  # new best score
+    if best_syn_data_path is not None:
+      try:
+        os.remove(best_syn_data_path)
+      except FileNotFoundError:
+        print(f'failed to delete syn data {best_syn_data_path}')
+    best_fid = fid
+    best_epoch = epoch
+    best_syn_data_path = syn_data_path
+  else:  # best score in the past
+    try:
+      os.remove(syn_data_path)
+    except FileNotFoundError:
+      print(f'failed to delete syn data {syn_data_path}')
+  return best_fid, best_syn_data_path, best_epoch
+
 def main():
-  parser = argparse.ArgumentParser()
-
-  # PARAMS YOU LIKELY WANT TO SET
-  parser.add_argument('--seed', type=int, default=999)
-  parser.add_argument('--data', type=str, default='cifar10',
-                      choices=['cifar10', 'celeba', 'imagenet32', 'celeba64', 'imagenet64'])
-  parser.add_argument('--workers', type=int, default=2)
-  parser.add_argument('--batch_size', type=int, default=128)
-  parser.add_argument('--batch_size_grad_acc', type=int, default=128)
-  parser.add_argument('--nc', type=int, default=3)
-  parser.add_argument('--nz', type=int, default=100)
-  parser.add_argument('--ngf', type=int, default=64)
-  parser.add_argument('--ndf', type=int, default=64)
-
-  parser.add_argument('--n_epochs', type=int, default=20)
-  parser.add_argument('--lr_default', type=float, default=0.0002)
-  parser.add_argument('--lr_dis', type=float, default=None)
-  parser.add_argument('--lr_gen', type=float, default=None)
-  parser.add_argument('--beta1', type=float, default=0.5)
-  parser.add_argument('--n_gpu', type=int, default=1)
-  parser.add_argument('--train_gen_every_n_steps', '-gen_freq', type=int, default=1)
-
-  parser.add_argument('--target_eps', type=float, default=None)
-  parser.add_argument('--target_delta', type=float, default=1e-6)
-  parser.add_argument('--clip_norm', type=float, default=None)
-
-  parser.add_argument('--model', type=str, default='convnet', choices=['convnet', 'resnet'])
-  parser.add_argument('--exp_name', type=str)
-  parser.add_argument('--pretrain_exp', type=str, default=None)
-  parser.add_argument('--single_iter', action='store_true')
-  parser.add_argument('--local_data', action='store_true')
-
-  parser.add_argument('--gen_output', type=str, default='tanh', choices=['linear', 'tanh'])
-
-  arg = parser.parse_args()
-
+  arg = get_args()
   save_dir = os.path.join('../logs/dcgan/', arg.exp_name)
 
   os.makedirs(save_dir, exist_ok=True)
@@ -286,7 +362,23 @@ def main():
   random.seed(arg.seed)
   pt.manual_seed(arg.seed)
 
-  if arg.target_eps is not None:
+  checkpoint_path = os.path.join(save_dir, 'checkpoint.pt')
+  if os.path.exists(checkpoint_path):
+    start_epoch, noise_factor, best_fid, best_epoch, best_syn_data_path, opt_g_state, opt_d_state, \
+      net_g_state, net_d_state, global_step = load_checkpoint(checkpoint_path)
+  else:
+    start_epoch = 0
+    global_step = 0
+    noise_factor = None
+    best_fid = None
+    best_epoch = None
+    best_syn_data_path = None
+    opt_g_state = None
+    opt_d_state = None
+    net_g_state = None
+    net_d_state = None
+
+  if arg.target_eps is not None and noise_factor is None:
     n_samples_dict = {'cifar10': 50_000, 'imagenet32': 1_281_167, 'celeba': 202_599,
                       'imagenet64': 1_281_167, 'celeba64': 202_599}
     n_samples = n_samples_dict[arg.data]
@@ -323,30 +415,29 @@ def main():
   device = pt.device("cuda:0" if (pt.cuda.is_available() and arg.n_gpu > 0) else "cpu")
 
   if arg.model == 'resnet':
-
     net_d, net_g = get_resnets(arg.nz, arg.ngf, arg.ndf, device, img_hw, arg.gen_output)
   else:
     assert img_hw == 32
     net_d, net_g = get_convnets(arg, device)
-
-  if arg.pretrain_exp is not None:
-    weights_file = os.path.join('../logs/dcgan/', arg.pretrain_exp, 'ck.pt')
+  if net_g_state is not None and net_d_state is not None:
+    net_g.load_state_dict(net_g_state)
+    net_d.load_state_dict(net_d_state)
+    del net_g_state
+    del net_d_state
+    print('loaded model weights from checkpoint')
+  elif arg.pretrain_exp is not None:
+    weights_file = os.path.join('../logs/dcgan/', arg.pretrain_checkpoint)
     pretrained_weights = pt.load(weights_file)
-    net_g.load_state_dict(pretrained_weights['g'])
-    net_d.load_state_dict(pretrained_weights['d'])
+    net_g.load_state_dict(pretrained_weights['net_g'])
+    net_d.load_state_dict(pretrained_weights['net_d'])
     del pretrained_weights
+    print('loaded model weights from pre-trained model')
 
   criterion = nn.BCEWithLogitsLoss()
 
   def disc_loss(x, y):
     return criterion(net_d(x).view(-1), y)
 
-  # Create batch of latent vectors that we will use to visualize
-  #  the progression of the generator
-
-  # Establish convention for real and fake labels during training
-
-  # Setup Adam optimizers for both G and D
   lr_dis = arg.lr_default if arg.lr_dis is None else arg.lr_dis
   lr_gen = arg.lr_default if arg.lr_gen is None else arg.lr_gen
 
@@ -355,23 +446,27 @@ def main():
     criterion = extend(criterion)
     # net_g = extend(net_g, use_converter=True)
 
-  optimizer_d = optim.Adam(net_d.parameters(), lr=lr_dis, betas=(arg.beta1, 0.999))
+  opt_d = optim.Adam(net_d.parameters(), lr=lr_dis, betas=(arg.beta1, 0.999))
+  opt_g = optim.Adam(net_g.parameters(), lr=lr_gen, betas=(arg.beta1, 0.999))
 
-  optimizer_g = optim.Adam(net_g.parameters(), lr=lr_gen, betas=(arg.beta1, 0.999))
-
+  if opt_g_state is not None and opt_d_state is not None:
+    net_g.load_state_dict(opt_g_state)
+    net_d.load_state_dict(opt_d_state)
+    del opt_g_state
+    del opt_d_state
+    print('loaded optimizer weights from checkpoint')
   # Training Loop
 
   # Lists to keep track of progress
   # img_list = []
   g_losses = []
   d_losses = []
-  iters = 0
 
   print("Starting Training Loop...")
-  global_step = 0
+
   err_g = None
   real_pred_acc, fake_pred_acc = None, None
-  for epoch in range(arg.n_epochs):
+  for epoch in range(start_epoch, arg.n_epochs):
     for i, data in enumerate(dataloader, 0):
       global_step += 1
       ############################
@@ -385,12 +480,12 @@ def main():
       disc_label = pt.full((b_size,), REAL_LABEL, dtype=pt.float, device=device)
 
       if arg.clip_norm is None:
-        res = nondp_update(net_d, net_g, optimizer_d, optimizer_g, real_batch, criterion,
+        res = nondp_update(net_d, net_g, opt_d, opt_g, real_batch, criterion,
                            disc_label, arg, b_size, device,
                            global_step)
         real_pred_acc, fake_pred_acc, err_d_item, err_g = res
       else:  # PRIVATE SETTING
-        dp_update(net_d, net_g, optimizer_d, optimizer_g, real_batch, criterion,
+        dp_update(net_d, net_g, opt_d, opt_g, real_batch, criterion,
                   disc_loss, disc_label, arg, b_size, device,
                   global_step, noise_factor)
         err_d_item = 0
@@ -402,57 +497,48 @@ def main():
           print(f'real_pred_acc {real_pred_acc}, fake pred acc {fake_pred_acc}')
         else:
           print(f'err_d {err_d_item}, err_g {err_g_item}')
-      #   print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
-      #         % (epoch, arg.n_epochs, i, len(dataloader),
-      #            err_d_item, err_g_item, D_x, D_G_z1, D_G_z2))
-
-      # if i % 500 == 0:
-      #   syn_data_path = create_fid_dataset(net_g, arg.nz, device, save_dir,
-      #                                      file_name=f'synth_data_{i}')
-      #   fid = get_fid_scores(syn_data_path, dataset_name=arg.data, device=device, n_samples=50_000,
-      #                        image_size=32, center_crop_size=32, data_scale='normed',
-      #                        batch_size=100)
-      #   print(f'fid at it {i}: {fid}')
-      #   os.remove(syn_data_path)
 
       # Save Losses for plotting later
       g_losses.append(err_g_item)
       d_losses.append(err_d_item)
 
-      iters += 1
-
       if arg.single_iter:
         break
+
+    if arg.single_iter or (epoch + 1) % arg.n_save_epochs == 0 or epoch + 1 == arg.n_epochs:
+      create_checkpoint(save_dir, epoch, noise_factor, best_fid, best_epoch, best_syn_data_path,
+                        opt_g, opt_d, net_g, net_d, global_step)
+      # make an image
+      fake = net_g(get_gen_noise(100, arg.nz, device)).detach().cpu()
+      print_fake_batch(arg, fake, data_scale, save_dir, file_name=f'clamped_plot_ep{epoch}.png')
+      # create a dataset
+      syn_data_path = create_fid_dataset(net_g, arg.nz, device, save_dir, file_name=f'synth_data_ep{epoch}')
+      # perform an fid eval
+      fid = get_fid_scores_fixed(syn_data_path, dataset_name=arg.data, device=device, n_samples=50_000,
+                                 image_size=img_hw, center_crop_size=img_hw, data_scale=data_scale, batch_size=100)
+      print(f'fid={fid} at epoch={epoch}')
+      np.save(os.path.join(save_dir, f'fid_ep{epoch}.npy'), fid)
+
+      best_fid, best_syn_data_path, best_epoch = update_best_score(fid, syn_data_path, epoch, best_fid,
+                                                                   best_syn_data_path, best_epoch)
+
+    if (epoch + 1) % arg.n_save_epochs == 0:
+      # sign off with error code 3
+      print(f'preparing restart after epoch {epoch}')
+      exit(3)
+
     if arg.single_iter:
-      break
+      exit(0)
 
-  fake = net_g(get_gen_noise(100, arg.nz, device)).detach().cpu()
-  if arg.data in {'cifar10', 'celeba'}:
-    if data_scale == 'normed':
-      mean_tsr = pt.tensor(IMAGENET_MEAN, device=fake.device)
-      sdev_tsr = pt.tensor(IMAGENET_SDEV, device=fake.device)
-      data_to_print = fake * sdev_tsr[None, :, None, None] + mean_tsr[None, :, None, None]
-    else:
-      data_to_print = fake
-  else:
-    mean_tsr = pt.tensor([0.5, 0.5, 0.5], device=fake.device)
-    sdev_tsr = pt.tensor([0.5, 0.5, 0.5], device=fake.device)
-    data_to_print = fake * sdev_tsr[None, :, None, None] + mean_tsr[None, :, None, None]
 
-  data_to_print = pt.clamp(data_to_print, min=0., max=1.)
-  img_path_clamp = os.path.join(save_dir, 'clamped_plot.png')
-  vutils.save_image(data_to_print, img_path_clamp, normalize=True, nrow=10)
-  final_images = vutils.make_grid(fake, padding=2, normalize=True, scale_each=True)
 
-  pt.save({'fake': fake}, os.path.join(save_dir, 'fake_final.pt'))
-  pt.save({'d': net_d.state_dict(), 'g': net_g.state_dict()}, os.path.join(save_dir, 'ck.pt'))
-  syn_data_path = create_fid_dataset(net_g, arg.nz, device, save_dir, file_name='synth_data')
-  fid = get_fid_scores_fixed(syn_data_path, dataset_name=arg.data, device=device, n_samples=50_000,
-                       image_size=img_hw, center_crop_size=img_hw, data_scale=data_scale, batch_size=100)
-  print(f'fid={fid}')
-  np.save(os.path.join(save_dir, 'fid.npy'), fid)
-  os.remove(syn_data_path)
-  make_final_plots(dataloader, g_losses, d_losses, device, final_images, save_dir)
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
